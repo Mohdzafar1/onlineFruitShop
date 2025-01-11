ee44b6d8f5ec939c589aae284c2a6639c3fe41fe
Mohd zafar <68924688+Mohdzafar1@users.noreply.github.com>
2025-01-11 16:13:36 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -15,7 +15,7 @@ const PORT=8080 || process.env.PORT
 
 
 var corsOptions = {
-  origin: ["http://localhost:3000","http://localhost:3001","https://online-fruit-shop-frontend.vercel.app/"],
+  origin: ["http://localhost:3000","http://localhost:3001","https://online-fruit-shop-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   optionsSuccessStatus: 200
 };
